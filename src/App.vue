82b8ec51b53621c661ffc4c5682be77cc471c84d
Diamsyah M Dida <didadiamsyah02@gmail.com>
2020-03-26 12:40:33 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
     <div class="container marginTop">
-      <h4>
-        Data COVID-19
-        <small>by Diamsyah M Dida</small>
-      </h4>
+      <h4>Data COVID-19</h4>
+      <pre>by</pre>
+      <h5><pre>Diamsyah M Dida</pre></h5>
       <br />
-      <div class="alert alert-success" role="alert">
-        <h5 class="alert-heading">Update Terakhir</h5>
+      <div class="alert alert-primary" role="alert">
+        <h5 class="alert-heading">Pembaharuan!</h5>
         <hr />
         <p class="mb-0">
-          Data ini di update terakhir kali pada
+          Data ini di perbaharui pada:
           <strong>{{allCovid.lastUpdate}}</strong>
         </p>
       </div>
@@ -20,7 +19,7 @@
       <div class="form-group row">
         <div class="col-md-1"></div>
         <label for="selectCountry" class="col-3 col-md-1">
-          <b>Country</b>
+          <b>Negara</b>
         </label>
         <select
           class="form-control col-6 col-md-8"
@@ -28,33 +27,28 @@
           @change="getCovidById"
           id="selectCountry"
         >
+          <option value>-- Pilih negara --</option>
           <option
             v-for="data in countries"
-            :key="data.id"
-            v-bind:value="data.code"
-          >{{data.country}} - {{data.code}}</option>
+            :key="data.iso2"
+            v-bind:value="data.iso2"
+          >{{data.name}} - {{data.iso2}}</option>
         </select>
         <div class="col-3 col-md-1">
-          <button class="btn btn-secondary btn-block" @click="reset">
-            <i class="fa fa-window-close"></i>
-          </button>
+          <span class="badge badge-secondary" @click="reset">
+            <!-- <i class="fa fa-window-close"></i> -->
+            Reset
+          </span>
         </div>
       </div>
     </form>
-    <br />
-    <button class="btn btn-primary" @click="showAll" v-if="!getAll">
-      <i class="fa fa-eye"></i> Show All Data
-    </button>
-    <i v-if="getAll">Please wait...</i>
-    <br />
-    <br />
     <div class="container" v-if="selectCountry.confirmed == ''">
       <h4>Total Data</h4>
       <div class="row">
         <div class="col-12 col-md-4">
           <div class="card text-center">
             <div class="card-header text-light bg-info">
-              Confirmed
+              Terkonfirmasi
               <i class="fa fa-check-square"></i>
             </div>
             <div class="card-body">
@@ -65,7 +59,7 @@
         <div class="col-12 col-md-4">
           <div class="card text-center">
             <div class="card-header text-light bg-success">
-              Recovered
+              Sembuh
               <i class="fa fa-retweet"></i>
             </div>
             <div class="card-body">
@@ -76,7 +70,7 @@
         <div class="col-12 col-md-4">
           <div class="card text-center">
             <div class="card-header text-light bg-danger">
-              deaths
+              Meninggal
               <i class="fa fa-ambulance"></i>
             </div>
             <div class="card-body">
@@ -92,7 +86,7 @@
         <div class="col-12 col-md-4">
           <div class="card text-center">
             <div class="card-header text-light bg-info">
-              Confirmed
+              Terkonfirmasi
               <i class="fa fa-check-square"></i>
             </div>
             <div class="card-body">
@@ -103,7 +97,7 @@
         <div class="col-12 col-md-4">
           <div class="card text-center">
             <div class="card-header text-light bg-success">
-              Recovered
+              Sembuh
               <i class="fa fa-retweet"></i>
             </div>
             <div class="card-body">
@@ -114,7 +108,7 @@
         <div class="col-12 col-md-4">
           <div class="card text-center">
             <div class="card-header text-light bg-danger">
-              Deaths
+              Meninggal
               <i class="fa fa-ambulance"></i>
             </div>
             <div class="card-body">
@@ -124,23 +118,66 @@
         </div>
       </div>
     </div>
+    <div class="container">
+      <h4>Tampilkan Data</h4>
+      <div class="row">
+        <div class="col-6 col-md-6">
+          <button class="btn btn-primary" @click="showAll" v-if="!getAll">Se-dunia</button>
+          <i v-if="getAll">Tunggu bentar...</i>
+        </div>
+        <div class="col-6 col-md-6">
+          <button
+            class="btn btn-warning text-light"
+            @click="showByProvinsiIndo"
+            v-if="!getByProv"
+          >Indonesia Saja</button>
+          <i v-if="getByProv">Tunggu bentar...</i>
+        </div>
+      </div>
+    </div>
     <div class="container" v-if="allData.length != 0">
       <div class="table table-responsive">
         <table class="table table-bordered">
           <thead class="thead-light">
             <tr>
-              <th>COUNTRY</th>
-              <th>CONFIRMED</th>
-              <th>RECOVERED</th>
-              <th>DEATHS</th>
+              <th>NEGARA</th>
+              <th>TERKONFIRMASI</th>
+              <th>SEMBUH</th>
+              <th>MENINGGAL</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="all in allData" :key="all.attributes.OBJECTID">
-              <td><b>{{all.attributes.Country_Region}}</b></td>
+              <td>
+                <b>{{all.attributes.Country_Region}}</b>
+              </td>
               <td>{{all.attributes.Confirmed}}</td>
               <td>{{all.attributes.Recovered}}</td>
               <td>{{all.attributes.Deaths}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="container" v-if="byProv.length != 0">
+      <div class="table table-responsive">
+        <table class="table table-bordered">
+          <thead class="thead-light">
+            <tr>
+              <th>PROVINSI</th>
+              <th>TERKONFIRMASI</th>
+              <th>SEMBUH</th>
+              <th>MENINGGAL</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="prov in byProv" :key="prov.attributes.FID">
+              <td>
+                <b>{{prov.attributes.Provinsi}}</b>
+              </td>
+              <td>{{prov.attributes.Kasus_Posi}}</td>
+              <td>{{prov.attributes.Kasus_Semb}}</td>
+              <td>{{prov.attributes.Kasus_Meni}}</td>
             </tr>
           </tbody>
         </table>
@@ -153,13 +190,14 @@
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import axios from "axios";
 import Vue from "vue";
+import VueAxios from "vue-axios";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 // Install BootstrapVue
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(BootstrapVueIcons);
-Vue.use(axios);
+Vue.use(VueAxios, axios);
 export default {
   name: "app",
   components: {},
@@ -194,8 +232,12 @@ export default {
       // variable untuk get semua data corona dan negara
       allData: [],
 
+      // variable untuk get data corona by provinsi di indo
+      byProv: [],
+
       // variable loading
-      getAll: false
+      getAll: false,
+      getByProv: false
     };
   },
   mounted() {
@@ -216,45 +258,42 @@ export default {
     // get semua data negara
     getCountries() {
       axios.get(this.apiURL + "countries").then(response => {
-        let countries = response.data.countries;
-
-        Object.entries(countries).map(([negara, kode]) => {
-          let data = {
-            id: this.index++,
-            country: negara,
-            code: kode
-          };
-          this.countries.push(data);
-        });
+        this.countries = response.data.countries;
       });
     },
 
     // get data korban corona by negara
     getCovidById() {
-      axios.get(this.apiURL + "countries/" + this.country).then(
-        response => {
-          this.selectCountry.confirmed = response.data.confirmed.value;
-          this.selectCountry.recovered = response.data.recovered.value;
-          this.selectCountry.deaths = response.data.deaths.value;
-        },
-        error => {
-          if (error.response) {
-            alert("Data not found");
-            location.reload();
+      if (this.country == "") {
+        this.reset();
+      } else {
+        axios.get(this.apiURL + "countries/" + this.country).then(
+          response => {
+            this.selectCountry.confirmed = response.data.confirmed.value;
+            this.selectCountry.recovered = response.data.recovered.value;
+            this.selectCountry.deaths = response.data.deaths.value;
+          },
+          error => {
+            if (error.response) {
+              alert("Data not found");
+              this.reset();
+            }
           }
-        }
-      );
+        );
+      }
     },
 
     // reset data
     reset() {
       this.selectCountry.confirmed = "";
+      this.country = '';
       this.getAllCovid();
     },
 
     // get semua data korban corona dan negara
     showAll() {
       this.getAll = true;
+      this.byProv = [];
       axios.get(this.apiURL2).then(
         response => {
           this.getAll = false;
@@ -268,17 +307,40 @@ export default {
           }
         }
       );
+    },
+
+    // get data korban corona by provinsi di indonesia
+    showByProvinsiIndo() {
+      this.getByProv = true;
+      this.allData = [];
+      axios.get("https://api.kawalcorona.com/indonesia/provinsi/").then(
+        response => {
+          this.getByProv = false;
+          this.byProv = response.data;
+        },
+        error => {
+          this.getByProv = false;
+          if (error.response) {
+            alert("Data not found");
+            location.reload();
+          }
+        }
+      );
     }
   }
 };
 </script>
 
 <style>
+body {
+  background-color: #fff;
+}
 .marginTop {
   margin-top: 50px;
 }
 
-.col-md-4 {
+.col-md-4,
+.col-6 {
   margin-bottom: 20px;
 }
 
@@ -292,5 +354,11 @@ table {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.badge{
+  padding: 8px 10px 8px 10px;
+  margin-top: 4px;
+  cursor: pointer;
 }
 </style>
